03320f7cbfa68ee380f4c88024ac21d87e4e3b9c
vralle <vitaliy.ralle@gmail.com>
2025-02-25 09:22:28 +0100

    feat: Style linting for TailwindCSS example

--- projects/tailwindcss-postcss/.stylelintrc.mjs
+++ projects/tailwindcss-postcss/.stylelintrc.mjs
@@ -3,8 +3,27 @@
  * @see https://stylelint.io/user-guide/configure/
  * @type {import('stylelint').Config}
  */
-export default {
+const stylelintConfig = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-tailwindcss",
+  ],
+  cache: true,
   ignoreFiles: [
-    "**/*",
+    "dist/**",
+    "node_modules/**",
+  ],
+  overrides: [
+    {
+      files: [
+        "**/*.scss",
+      ],
+      extends: [
+        "stylelint-config-standard-scss",
+        "stylelint-config-tailwindcss/scss",
+      ],
+    },
   ],
 };
+
+export default stylelintConfig;
